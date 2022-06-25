38e055bba9259bd8f05435f27ef15abae58d7d67
umar <umarjan260797@gmail.com>
2022-06-25 23:01:10 +0300

    написал оставшиеся проверки

--- hello/webapp/views.py
+++ hello/webapp/views.py
@@ -34,6 +34,35 @@ def check2(num):
         return ii
 
 
+def check3(num):
+    sets = set(num)
+    if len(num) == len(sets):
+        return num
+    else:
+        raise ErrorValueSet()
+
+
+def check4(num, secret_numbers):
+    calculate = 0
+    for i in num:
+        if i > 0:
+            calculate += 1
+    if calculate > len(secret_numbers) or calculate < len(secret_numbers):
+        raise ErrorValueDeficiency()
+
+
+def check5(num, secret_numbers):
+    number = 0
+    number2 = 0
+    for u, i in enumerate(secret_numbers):
+        for x, y in enumerate(num):
+            if id == x and y == i:
+                number += 1
+            elif y == i:
+                number2 += 1
+    return number, number2
+
+
 def index_view(request):
     if request.method == "GET":
         return render(request, "index.html")
